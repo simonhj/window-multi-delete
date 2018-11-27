ccffbf718b6a3f846747aeee44f5e0bae5133627
root <root@LAPTOP-THPKIMHI.localdomain>
2018-11-26 20:34:47 -0800

    buffer updating figured out

--- window-closer.el
+++ window-closer.el
@@ -14,3 +14,13 @@
 	    (with-current-buffer (window-buffer win)
 	      `(,win . ,mode-line-front-space)))
 	  (window-list)))
+
+(defun append-id (win-list)
+  (let ((win-idx 0))
+    (dolist (win win-list)
+      (with-current-buffer (window-buffer win)
+	(setq-local mode-line-front-space  (concat "W: " (number-to-string win-idx)))
+	(setq win-idx (+ win-idx 1))
+	(force-mode-line-update t)))))
+
+(append-id (window-list))

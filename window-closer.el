027d226d7d143aa90816f45e5f5666e34c99b9da
root <root@LAPTOP-THPKIMHI.localdomain>
2018-11-29 20:03:10 -0800

    MVP!

--- window-closer.el
+++ window-closer.el
@@ -1,19 +1,10 @@
-;(dolist (win (window-list) nil)
-  
-
-(walk-windows
- (lambda (win)
-   (let ((buf (window-buffer win)))
-     (with-current-buffer buf
-       (progn (setq mode-line-front-space "W: 1")
-	      (force-mode-line-update 1))))))
-	
-
 (defun save-mode-prefix (win-list)
   (mapcar (lambda (win)
 	    (with-current-buffer (window-buffer win)
 	      `(,win . ,mode-line-front-space)))
 	  win-list))
+
+(save-mode-prefix (window-list))
 
 (defvar oldies (save-mode-prefix (window-list)))
 
@@ -28,11 +19,12 @@
 
 (defun restore-prefs (old-vals)
   (dolist (ac old-vals)
-    (let ((buf (window-buffer (car ac)))
-	  (old (cdr ac)))
-      (with-current-buffer buf
-	(setq-local mode-line-front-space old)
-	(force-mode-line-update)))))
+    (when (window-live-p (car ac))
+      (let ((buf (window-buffer (car ac)))
+	    (old (cdr ac)))
+	(with-current-buffer buf
+	  (setq-local mode-line-front-space (if (equal old nil) "" old))
+	  (force-mode-line-update))))))
 
 (defun keystroke-to-int (ks)
   (let ((val (- ks 48)))
@@ -41,15 +33,18 @@
       nil)))
 
 (defun read-window ()
-  (keystroke-to-int (read-char "id of window to delete?")))
+  (keystroke-to-int (read-char "id of window to delete or q to quit?")))
 
 (defun kill-loop (win-list)
-  (let ((save-mode-prefix win-list))
+  (let ((old (save-mode-prefix win-list)))
     (append-id win-list)
-    (let ((win
-    (while 
-  
-
+    (let ((win (read-window)))
+      (while (not (equal win nil))
+	(delete-window (nth win win-list))
+	(setq win (read-window))))
+    (restore-prefs old)))
+	
+(kill-loop (window-list))
 
 (defun window-multi-kill ()
   (let* ((windows (window-list))
